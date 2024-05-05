859fdcd3d9be330afb4a87ef6154bf9ffeb2d0e0
larisasirokih <Lorashirokikh@yandex.ru>
2024-05-05 19:55:44 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  env: {
-    API_URL: process.env.API_URL
-  },
+  publicPath: process.env.NODE_ENV === "production" 
+  ? "/my-project/" : "/",
 });
