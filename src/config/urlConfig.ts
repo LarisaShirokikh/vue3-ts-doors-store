f6b05fbb507b3b31a26562038d83286569cf0cd1
larisasirokih <Lorashirokikh@yandex.ru>
2024-05-11 23:21:37 +0300

    url

--- src/config/urlConfig.ts
+++ src/config/urlConfig.ts
@@ -1,21 +1,6 @@
-// const config: UrlConfig = {
-//   apiUrl: "https://api.dverietalon.ru",
-//   API_URL: "https://api.dverietalon.ru/api",
-//   ALLOWED_ORIGIN: "https://api.dverietalon.ru",
-// };
-
-// export default config;
-
-// interface UrlConfig {
-//   apiUrl: string;
-//   API_URL: string;
-//   ALLOWED_ORIGIN: string;
-// }
-
-
 const config: UrlConfig = {
-  apiUrl: "http://localhost:4200",
-  API_URL: "http://localhost:4200/api",
+  apiUrl: "https://api.dverietalon.ru",
+  API_URL: "https://api.dverietalon.ru/api",
   ALLOWED_ORIGIN: "https://api.dverietalon.ru",
 };
 
@@ -26,3 +11,18 @@ interface UrlConfig {
   API_URL: string;
   ALLOWED_ORIGIN: string;
 }
+
+
+// const config: UrlConfig = {
+//   apiUrl: "http://localhost:4200",
+//   API_URL: "http://localhost:4200/api",
+//   ALLOWED_ORIGIN: "https://api.dverietalon.ru",
+// };
+
+// export default config;
+
+// interface UrlConfig {
+//   apiUrl: string;
+//   API_URL: string;
+//   ALLOWED_ORIGIN: string;
+// }
